4681f07fcf80b34c35b3237a4740d22a62c182b6
YUDHVEER SINGH <yuvigurjar1001@gmail.com>
2024-08-01 14:48:27 +0530

    Add files via upload

---
+++ PRE-DEFINED_FUNCTION.py
@@ -0,0 +1,15 @@
+#PRE-DEFIND FUNCTION
+print("WELCOME TO FUNCTION!")
+
+print(max(5,7,2))
+print(min(5,7,2))
+c=(3,4,5,7)
+print("SUM=",sum(c))
+print("MAX=",max(c))
+print("MIN=",min(c))
+
+print(abs(5.3))
+
+a=(1,2,3,4,4,5,6)
+
+print(a.count(4))
---
+++ TUPLE.py
@@ -0,0 +1,7 @@
+#Author: YUDHVEER SINGH
+#tuple
+mytuple=("AMIT","POOJA","RAJ","ROHIT")
+print(mytuple)
+print(mytuple[0])
+print(mytuple[1:])
+print(mytuple[:3])
