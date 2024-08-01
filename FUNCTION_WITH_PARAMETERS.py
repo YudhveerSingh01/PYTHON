1c4fb56c888229af59723b80e66e978a71877058
YUDHVEER SINGH <yuvigurjar1001@gmail.com>
2024-08-01 14:49:47 +0530

    Add files via upload

---
+++ FUNCTION_WITH_PARAMETERS.py
@@ -0,0 +1,19 @@
+#FUNCTION WITH PARAMETER
+def myFun(message):
+    print(message)
+
+myFun("welcome")
+
+def find_sum(a,b):
+
+    print(a+b)
+
+find_sum(9,7)
+
+def sum(x,y):
+    return x+y
+
+x=int(input("ENTER FIRST NUMBER ="))
+y=int(input("ENTER SECOND NUMBER="))
+z=sum(x,y)
+print("sum=",z)
