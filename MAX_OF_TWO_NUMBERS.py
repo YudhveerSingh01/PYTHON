564a7316759ea7c5ca04be7dfccd0e1f0bff12fb
YUDHVEER SINGH <yuvigurjar1001@gmail.com>
2024-08-05 09:36:39 +0530

    Add files via upload

---
+++ MAX_OF_TWO_NUMBERS.py
@@ -0,0 +1,11 @@
+a=input("enter value of a:")
+b=input("enter value of b:")
+if(a>b):
+  print("a is greater than b ")
+else:
+  print("b is greater")
+#using max() function
+x=input("enter value of x")
+y=input("enter value of y")
+maximum=max(x,y)
+print(maximum)
